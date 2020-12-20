aa23d1d480e1e24d706f6b75787bb84193c1825d
hearttmy <1156609896@qq.com>
2020-12-20 20:40:41 +0800

    init

---
+++ src/components/common/BaseHeader.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+name: "BaseHeader"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  configureWebpack: {
+    alias: {
+      'assets': '@/assets',
+      'common': '@/common',
+      'components': '@/components',
+      'network': '@/network',
+      'views': '@/views',
+    }
+  }
+}
