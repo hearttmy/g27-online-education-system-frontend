8a5a59c3d36f8fb78745c1fc9852063f8938bfc3
unknown <1156609896@qq.com>
2021-01-12 14:42:21 +0800

    v7.0

--- src/common/config.js
+++ src/common/config.js
@@ -1,5 +1,5 @@
-// const BaseUrl = 'http://121.196.154.81:2088'
-const BaseUrl = 'http://10.181.184.119:2088'
+const BaseUrl = 'http://121.196.154.81:2088'
+// const BaseUrl = 'http://10.181.184.119:2088'
 export default {
   serverBaseUrl: BaseUrl,
   serverApiUrl: BaseUrl + '/api',
